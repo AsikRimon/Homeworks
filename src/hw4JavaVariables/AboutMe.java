7d03e217a47067ff9bb34edcd32d7187ee8f5e99
asikrimon <asikrimonr@gmail.com>
2022-05-19 11:05:04 -0700

    Changed at hw

--- src/hw4JavaVariables/AboutMe.java
+++ src/hw4JavaVariables/AboutMe.java
@@ -1,41 +1,28 @@
 package hw4JavaVariables;
 
-
-
 public class AboutMe {
 	// variables declared
-		public String name;
-		public byte age;
-		public short myApartmentRent;
-		public int myYearlySalary;
-		public long myBankBalance;
-		public float myHeight;
-		public double myGrade;
-		public char sex;
-		public boolean usCitizen;
-		
-		
-		
-		public AboutMe () {
-			System.out.println("\n This is all about us --\n");
-			
-		}
-		
-			
-			
-			public void AboutMe () {
-			System.out.println("My Name: " + name + "\nMy Age: " + age + "\nMy HouseRent: " + myApartmentRent
-					+ "\nYearly Salary: " + myYearlySalary + "\nMy Bank Balance: " + myBankBalance + "\nSex: " + sex
-					+ "\nMy Height: " + myHeight + "\nMy Grade: " + myGrade + "\nAm I US Citizen? Ans: " + usCitizen);
-			
-			}
-			
+	public String name;
+	public byte age;
+	public short myApartmentRent;
+	public int myYearlySalary;
+	public long myBankBalance;
+	public float myHeight;
+	public double myGrade;
+	public char sex;
+	public boolean usCitizen;
+
+	public AboutMe() {
+		System.out.println("\n This is all about us --\n");
+
+	}
+
+	public void AboutMe() {
+		System.out.println("My Name: " + name + "\nMy Age: " + age + "\nMy HouseRent: " + myApartmentRent
+				+ "\nYearly Salary: " + myYearlySalary + "\nMy Bank Balance: " + myBankBalance + "\nSex: " + sex
+				+ "\nMy Height: " + myHeight + "\nMy Grade: " + myGrade + "\nAm I US Citizen? Ans: " + usCitizen);
+		// changed
+
+	}
+
 }
-			
-			
-		
-
-
-		
-
-
--- src/hw4JavaVariables/AboutMeTest.java
+++ src/hw4JavaVariables/AboutMeTest.java
@@ -2,48 +2,32 @@ package hw4JavaVariables;
 
 public class AboutMeTest {
 	public static void main(String[] args) {
-		 AboutMe asik = new AboutMe ();
-		 
-		 asik.name = "Asik Rimon";
-		 asik.age = 23;
-		 asik.myApartmentRent = 2234;
-		 asik.myYearlySalary = 2324535;
-		 asik.myBankBalance =21222222;
-		 asik.myHeight = 6.0f;
-		 asik.myGrade = 3.5;
-		 asik.sex ='M';
-		 asik.usCitizen = true;
-		 asik.AboutMe();  // method initialized
-		 
-		 //About my friend Alex
-		 AboutMe alex =new AboutMe();
-		 alex.name ="Alex";
-		 alex.age =21;
-		 alex.myApartmentRent = 1200;
-		 alex.myYearlySalary =246678;
-		 alex.myBankBalance =211980998;
-		 alex.myHeight = 5.2f;
-		 alex.myGrade =3.5;
-		 alex.sex ='M';
+		AboutMe asik = new AboutMe();
+
+		asik.name = "Asik Rimon";
+		asik.age = 23;
+		asik.myApartmentRent = 2234;
+		asik.myYearlySalary = 2324535;
+		asik.myBankBalance = 21222222;
+		asik.myHeight = 6.0f;
+		asik.myGrade = 3.5;
+		asik.sex = 'M';
+		asik.usCitizen = true;
+		asik.AboutMe(); // method initialized
+
+		// About my friend Alex
+		AboutMe alex = new AboutMe();
+		alex.name = "Alex";
+		alex.age = 21;
+		alex.myApartmentRent = 1200;
+		alex.myYearlySalary = 246678;
+		alex.myBankBalance = 211980998;
+		alex.myHeight = 5.2f;
+		alex.myGrade = 3.5;
+		alex.sex = 'M';
 		alex.usCitizen = true;
-		 alex.AboutMe();// method initialized
-		 
-		 
-		 
-		
-		 
-		 
-		 
-		 
-		 
-		 
-		 
-		 
-		 
-		 
-		 
-		
+		alex.AboutMe();// method initialized
+		// Changed
 	}
-	
 
 }
